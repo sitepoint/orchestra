0f9a337afc52796b0eb08fd067a963323fe2c01c
Michael Sauter <michael.sauter@sitepoint.com>
2012-12-11 14:59:25 +1100

    Use better default values in config

--- config.php
+++ config.php
@@ -2,9 +2,10 @@
 $orchestraConfig = array(
     'database' => array(
         'driver' => 'pdo_mysql', // only MySQL is supported right now
-        'user' => 'root',
-        'password' => '',
-        'dbname' => 'wordpress'
+        'host' => DB_HOST,
+        'user' => DB_USER,
+        'password' => DB_PASSWORD,
+        'dbname' => DB_NAME
     ),
     'csrfSecret' => 'sxZ8vumGNx0x10d7a0GKbN8c0V5DZP', // replace with your secret
     'env' => 'dev', // set this to "prod" when using in production
